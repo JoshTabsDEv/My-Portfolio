1383f91e2882326557992a6def97651fdaf7aee3
JoshTabsDEv <tabura.joshua@cec.edu.ph>
2025-12-17 14:33:31 +0800

    first commit

---
+++ src/config.ts
@@ -0,0 +1,53 @@
+export const siteConfig = {
+  name: "Joshua Tabura",
+  title: "Web Developer Intern",
+  description: "Portfolio website of Joshua Tabura",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "your-email@example.com",
+    linkedin: "https://linkedin.com/in/yourprofile",
+    twitter: "https://x.com/rfitzio",
+    github: "https://github.com/RyanFitzgerald",
+  },
+  aboutMe:
+    "Aspiring IT Intern seeking hands-on experience in software development and technology operations. Eager to apply foundational skills in web development, AR technologies, and problem-solving while contributing to a dynamic and growth-oriented tech environment.",
+  skills: ["Javascript","Typescript" ,"React", "Node.js", "Laravel", "Express"],
+  projects: [
+    {
+      name: "Bohol Lens",
+      description:
+        "Developed an immersive augmented reality (AR) museum application that showcases Bohol’s cultural heritage through interactive digital exhibits. The project integrates AR-based navigation, 3D artifact visualization, and user-friendly interfaces to enhance visitor engagement and learning. Led the implementation of AR interactions, 3D model integration, and performance optimization for Android devices, ensuring a smooth and engaging on-site museum experience using Unity, AR Foundation, and Vuforia.",
+      link: "https://404-reality-not-found.vercel.app",
+      skills: ["Google AR Core", "C#", "AR Foundation"],
+    },
+    {
+      name: "Computer-Aided Learning Management System for Senior High School Students of Cristal e-College",
+      description:
+        "Developed a computer-aided learning management system designed to support modular instruction and academic activities for senior high school students. The system enabled efficient module distribution, assignment submission, and student progress tracking. Contributed to backend development, database structuring, and user interface implementation while collaborating with team members to ensure usability, reliability, and alignment with academic requirements.",
+      link: "https://github.com/JoshTabsDEv/computer-aided-model-system-for-student",
+      skills: ["Laravel", "Alphine.js", "Tailwind","Hostinger"],
+    },
+   
+  ],
+  experience: [
+    {
+      company: "Miru Systems Co., Ltd.",
+      title: "IT Support",
+      dateRange: "May 2025 - May 2025",
+      bullets: [
+        "Provided on-site IT support for the national election canvassing system during official election operations.",
+        "Ensured system stability, accuracy, and continuity of canvassing processes under time-critical conditions",
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "Cristal e-College",
+      degree: "Bachelor of Science in Information Technology",
+      dateRange: "2022 - Present",
+      achievements: [
+        "President of Cristal e-College Programmer's Guild",
+      ],
+    },
+  ],
+};
